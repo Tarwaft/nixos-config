a1fea07f8fe8e600de838938f3f2a324c07da302
Tarwaft <tarwaft@pm.me>
2025-05-10 15:15:25 +0200

    update

--- home/desktop/astal/widget/Bar.lua
+++ home/desktop/astal/widget/Bar.lua
@@ -1,9 +1,14 @@
 local Widget = require("astal.gtk3.widget")
 local Anchor = require("astal.gtk3").Astal.WindowAnchor
 local mButton = require("widget.MyButton")
-
+local inline_css = [[
+    window {
+        background-color: transparent;
+    }
+]]
 return function(monitor)
     return Widget.Window({
+        css = inline_css,
         monitor = monitor,
         anchor = Anchor.TOP + Anchor.LEFT + Anchor.RIGHT,
         exclusivity = "EXCLUSIVE",
