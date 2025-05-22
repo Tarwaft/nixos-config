65a5750521129b0222cd4044f54a092257cb2dda
tarwaft <tarwaft@pm.me>
2025-05-22 13:17:07 +0200

    added dark theme

--- home/system/gtk.nix
+++ home/system/gtk.nix
@@ -1,6 +1,11 @@
+{pkgs,...}:
 {
     gtk = {
         enable = true;
+        theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
         cursorTheme = {
             name = "BreezeX-RosePine-Linux";
             size = 24;
