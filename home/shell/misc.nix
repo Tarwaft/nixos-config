7adc502837f3e4f517f91ffc9b8df6e8a723d08c
Tarwaft <tarwaft@pm.me>
2025-05-29 15:42:07 +0200

    starship

--- home/shell/misc.nix
+++ home/shell/misc.nix
@@ -1,21 +1,25 @@
 {pkgs, inputs, system,...}:
 {
     home.packages = (with pkgs; [
+
+
+        # movies
         inputs.lobster.packages.${system}.lobster
         ani-cli
+
+
+        #pretty stuff
+        starship #for commandprompt
+
+        #utils
         curl
-
-
         killall
-
         zip
         xz
         unzip
         p7zip
-
         fzf
         git
-
         openssh
     ]);
 }
