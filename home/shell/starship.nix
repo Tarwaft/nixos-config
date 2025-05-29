5468737c17804ae41db1171185ff4bf8125010b8
Tarwaft <tarwaft@pm.me>
2025-05-29 16:28:28 +0200

    customization for starship

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -4,7 +4,7 @@
     enableBashIntegration = true;
     settings = {
       "$schema" = "https://starship.rs/config-schema.json";
-      format = "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$cpp$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda$pixi[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character";
+      format = "[](color_bg4)$os$username[](bg:color_lightblue fg:color_bg4)$directory[](fg:color_lightblue bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$cpp$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda$pixi[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character";
 
       palette = "gruvbox_dark";
 
@@ -13,17 +13,22 @@
         color_bg1 = "#3c3836";
         color_bg3 = "#665c54";
         color_blue = "#458588";
-        color_aqua = "#689d6a";
+        color_aqua = "#007d84";
         color_green = "#98971a";
         color_orange = "#d65d0e";
         color_purple = "#b16286";
         color_red = "#cc241d";
+        color_bg4 = "#71859b";
         color_yellow = "#d79921";
+        color_white = "#FFFFFF";
+        color_black = "#000000";
+        color_textblue = "#DDF3FE";
+        color_lightblue = "#6da1a3";
       };
 
       os = {
         disabled = false;
-        style = "bg:color_orange fg:color_fg0";
+        style = "bg:color_bg4 fg:color_black";
         symbols = {
           Linux = "󰌽";
           Artix = "󰣇";
@@ -32,13 +37,13 @@
 
       username = {
         show_always = true;
-        style_user = "bg:color_orange fg:color_fg0";
-        style_root = "bg:color_orange fg:color_fg0";
+        style_user = "bg:color_bg4 fg:color_textblue";
+        style_root = "bg:color_bg4 fg:color_red";
         format = "[ $user ]($style)";
       };
 
       directory = {
-        style = "fg:color_fg0 bg:color_yellow";
+        style = "fg:color_fg0 bg:color_lightblue";
         format = "[ $path ]($style)";
         truncation_length = 3;
         truncation_symbol = "…/";
