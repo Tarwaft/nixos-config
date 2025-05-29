47ea1e9415f4c9b3894f95b3d9f96c437745e55f
Tarwaft <tarwaft@pm.me>
2025-05-29 13:12:04 +0200

    exported bashrc to git

--- home/shell/bash.nix
+++ home/shell/bash.nix
@@ -2,10 +2,6 @@
     programs.bash = {
         enable = true;
         enableCompletion = true;
-        bashrcExtra = ''
-        alias nrs='sudo nixos-rebuild switch --flake ~/nixos-config/'
-        fastfetch
-        '';
-        
+        initExtra = builtins.readFile ./bashrc;
     };
 }
