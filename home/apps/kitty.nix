9db41252519dedf4c8ecb26f58fc4da0648755b2
Tarwaft <tarwaft@pm.me>
2025-06-19 17:58:57 +0200

    moved fastfetch to autostart in kitty instead of bashrc

--- home/apps/kitty.nix
+++ home/apps/kitty.nix
@@ -7,5 +7,8 @@
             window_margin_width = 1;
             font_family = "Monaspace Krypton";
         };
+        extraConfig = "
+        startup_session ~/nixos-config/home/apps/kitty_startup_session.conf
+        ";
     };
 }
