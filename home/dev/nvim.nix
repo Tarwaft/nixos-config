952dc3022709d64d29b73a23713c22854024b865
Tarwaft <tarwaft@pm.me>
2026-04-20 12:30:44 +0200

    fixed nvim

--- home/dev/nvim.nix
+++ home/dev/nvim.nix
@@ -1,21 +1,27 @@
-{pkgs,config,...}:
+{pkgs, config, ...}:
 {
     xdg.configFile."nvim".source =
-  config.lib.file.mkOutOfStoreSymlink "/home/tarwaft/nixos-config/home/dev/nvim";
-    programs.neovim = {
-        enable = true;
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
-        extraPackages = with pkgs; [
-            vimPlugins.nvim-treesitter-parsers.cpp
-            rust-analyzer 
-            ripgrep       
-            gcc
-            tree-sitter
-            fd
-            lua
-            stylua
-  ];
-};
+        config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/dev/nvim";
+
+    home.packages = with pkgs; [
+        neovim
+        vimPlugins.nvim-treesitter-parsers.cpp
+        rust-analyzer
+        ripgrep
+        gcc
+        tree-sitter
+        fd
+        lua
+        stylua
+    ];
+
+    home.sessionVariables = {
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+    };
+
+    home.shellAliases = {
+        vi = "nvim";
+        vim = "nvim";
+    };
 }
